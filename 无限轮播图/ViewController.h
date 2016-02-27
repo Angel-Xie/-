6dceacca0a096fdf7af986f8aa149fdb2ccca067
Angel-Xie <xiexiaoyu@xiexiaoyudeMacBook-Pro.local>
2016-02-27 19:10:21 +0800

    无限轮播图
    支持SDWebImage的一个无限轮播图,支持1到多张图片

---
+++ 无限轮播图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  无限轮播图
+//
+//  Created by 谢小御 on 16/1/8.
+//  Copyright © 2016年 谢小御. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
